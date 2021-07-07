1c549c50cc8022c689e1f724548ddf059c31476a
vbk1910 <78388087+vbk1910@users.noreply.github.com>
2021-07-07 10:03:31 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log ("My 1ST!!! Project on VSC");
+var first_name = "Vihaan Bhanot";
+var last_name = "Khurana";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 9;
+
+var add = a+b;
+console.log(add);
+
+var subtract = a-b;
+console.log(subtract);
+
+var multiply = a*b;
+console.log(multiply);
+
+var divide = a/b;
+console.log(divide);
